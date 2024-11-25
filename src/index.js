390e4b76470beb33eeb58f6b98950020cb4d1693
KUSHANK PATIL <patilkushank@gmail.com>
2024-11-25 13:58:27 +0530

    Added notification cron jobs (Emailservice)

--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@ const bodyparser = require('body-parser');
 const app = express();
 const {PORT} = require('./config/ServerConfig')
 const {sendBasicEmail} = require('./services/email-service')
+const jobs = require('./utils/job')
+const TicketController = require('./controller/ticket-controller')
+
 
 const setUpandStartServer = ()=>{
     app.use(bodyparser.json());
     app.use(bodyparser.urlencoded({extended:true}));
+    app.post('/api/v1/tickets',TicketController.create)
+    let currentTime = new Date();
     app.listen(PORT,()=>{
         console.log(`Server is running fine ${PORT}`);
-        sendBasicEmail(
-            'support@flyconnect@gmail.com',
-            'aerosphere911@gmail.com',
-            'Test Email',
-            'This is a test email'
-        )
+        console.log(currentTime);
+        jobs();
+        // sendBasicEmail(
+        //     'support@flyconnect@gmail.com',
+        //     'aerosphere911@gmail.com',
+        //     'Test Email',
+        //     'This is a test email'
+        // )
         });
 }
 setUpandStartServer();

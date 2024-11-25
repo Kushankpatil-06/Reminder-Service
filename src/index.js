5de79e21c4b330e1d265e05016238f2898b4153f
KUSHANK PATIL <patilkushank@gmail.com>
2024-11-25 20:23:21 +0530

    Added RabbitMQ for the Pub/Sub Model

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 const express = require('express');
-const bodyparser = require('body-parser');
-const app = express();
-const {PORT} = require('./config/ServerConfig')
-const {sendBasicEmail} = require('./services/email-service')
-const jobs = require('./utils/job')
-const TicketController = require('./controller/ticket-controller')
+const bodyParser = require('body-parser');
 
+const { PORT } = require('./config/serverConfig');
 
-const setUpandStartServer = ()=>{
-    app.use(bodyparser.json());
-    app.use(bodyparser.urlencoded({extended:true}));
-    app.post('/api/v1/tickets',TicketController.create)
-    let currentTime = new Date();
-    app.listen(PORT,()=>{
-        console.log(`Server is running fine ${PORT}`);
-        console.log(currentTime);
-        jobs();
-        // sendBasicEmail(
-        //     'support@flyconnect@gmail.com',
-        //     'aerosphere911@gmail.com',
-        //     'Test Email',
-        //     'This is a test email'
-        // )
-        });
+const TicketController = require('./controller/ticket-controller');
+const EmailService = require('./services/email-service');
+
+const jobs = require('./utils/job');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
+
+const setupAndStartServer = async () => {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.post('/api/v1/tickets', TicketController.create);
+
+    // const channel = await createChannel();
+    // subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+
+    app.listen(PORT, () => {
+        console.log(`Server started at port ${PORT}`);
+        // jobs();
+        
+    });
 }
-setUpandStartServer();
+
+setupAndStartServer();
